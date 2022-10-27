e1ada6f00d5f439714a54fc1cc1fcc129a4dbd93
luenra <lere1995@gmail.com>
2022-10-27 00:01:17 -0500

    background

--- editar_proveedor.php
+++ editar_proveedor.php
@@ -1,5 +1,4 @@
 <?php 
-    //var_dump($_GET);
 
     $servername = "localhost";
 	$username = "root";
@@ -18,7 +17,7 @@
 	$result = $conn->query($sql);
 
     $prov = $result->fetch_assoc();
-    var_dump($prov);
+    //var_dump($prov);
     ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -26,8 +25,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Proveedor</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
-<body>
-    
+<body >
+    <img src="resources/fondo.png" alt="Background" style="height:100vh ; width:100% ;background-attachment: fixed;">
+	
 </body>
 </html>
