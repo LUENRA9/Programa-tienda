77555bc224227da94dbfb86d80301e951e6e7994
luenra <lere1995@gmail.com>
2022-10-27 22:38:54 -0500

    adding css

--- editar_proveedor.php
+++ editar_proveedor.php
@@ -1,5 +1,5 @@
 <?php 
-
+	var_dump($_GET); 
     $servername = "localhost";
 	$username = "root";
 	$password = "2208";
@@ -27,9 +27,9 @@
     <title>Editar Proveedor</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css">
 </head>
-<body >
-    <img src="resources/fondo.png" alt="Background" style="height:100vh ; width:100% ;background-attachment: fixed;">
-	
+<body>
+    
 </body>
 </html>
